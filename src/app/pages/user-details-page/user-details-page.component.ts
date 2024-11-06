001325cacef5398156c9dd7926b0c6500b9aa731
Vladimer-Snkhchyan <vladimir.snkhchyan@optimumpartners.co>
2024-11-06 13:50:10 +0400

    error fix

--- src/app/pages/user-details-page/user-details-page.component.ts
+++ src/app/pages/user-details-page/user-details-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, inject} from '@angular/core';
-import {toSignal} from '@angular/core/rxjs-interop';
 import {FormBuilder, FormControl, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
@@ -7,6 +6,7 @@ import { User } from '../../interfaces/user';
 import { MatFormField,MatLabel, MatFormFieldModule, MatFormFieldControl} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
+import { UserRetrieveService } from '../../services/user-retrieve.service';
 
 @Component({
   selector: 'app-user-details-page',
@@ -23,85 +23,62 @@ import {MatSelectModule} from '@angular/material/select';
   styleUrl: './user-details-page.component.scss'
 })
 export class UserDetailsPageComponent implements OnInit {
+  id!: number;
+  user!: User | undefined;
 
+  email = new FormControl(' ',[Validators.required, Validators.email]);
+  first_name= new FormControl('', Validators.required);
+  middle_name= new FormControl('');
+  last_name= new FormControl('', Validators.required);
+  date_of_birth= new FormControl('');
+  is_activated= new FormControl(false);
+  profile_img_url= new FormControl('');
+  phone_number= new FormControl('', Validators.pattern(/^\d+$/));
+  main_language= new FormControl('');
+  nationality= new FormControl('');
+  recitations= new FormControl('');
 
-  users: User[] = [];
-  private url_to_db = './assets/db/users.json';
-  userForm!: FormGroup;
-  id!: number | null ;
-
-
-  constructor (private route: ActivatedRoute, private http: HttpClient) {};
+  constructor (private route: ActivatedRoute, private http: HttpClient, private userRetrieveService: UserRetrieveService) {
+    this.route.queryParams.subscribe(params => {
+      this.id = +params['id'];
+      this.checkId();
+      console.log('ID:', this.id);
+    })
+  };
 
   ngOnInit(): void {
-    this.loadUsers();
-
     this.route.queryParams.subscribe(params => {
       this.id = +params['id'];
       console.log('ID:', this.id);
-      this.checkId();
-    });
-
-    this.userForm = new FormGroup({
-      id: new FormControl(0),
-      first_name: new FormControl('', Validators.required),
-      middle_name: new FormControl(''),
-      last_name: new FormControl('', Validators.required),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      date_of_birth: new FormControl(''),
-      is_activated: new FormControl(false),
-      profile_img_url: new FormControl(''),
-      phone_number: new FormControl('', Validators.pattern(/^\d+$/)),
-      gender: new FormControl(true),
-      main_language: new FormControl(''),
-      nationality: new FormControl(''),
-      recitations: new FormControl(''),
-    });
-
-
+    })
+    this.checkId();
   }
 
   private checkId() {
       // Check if the user exists in the users array
-      const user = this.users.find(user => user.id === this.id);
-      
-      if (user) {
-        // patch the form with the user's data
-        //this.patchForm(user);
-      } else {
+      this.userRetrieveService.getUserById(this.id).subscribe(
+        user => {if(user){ this.user = user;}
+                  else { this.user = undefined}          
+      },
+        error => console.error('Error fetching user:', error)
+      );
+     
+      if (this.user) {
+        this.email.setValue(this.user.email);
+        this.first_name.setValue(this.user.first_name);
+        this.middle_name.setValue(this.user.middle_name);
+        this.last_name.setValue(this.user.last_name);
+        this.date_of_birth.setValue(this.user.date_of_birth);
+        this.is_activated.setValue(this.user.is_activated);
+        this.profile_img_url.setValue(this.user.profile_img_url);
+        this.phone_number.setValue(this.user.phone_number);
+        this.main_language.setValue(this.user.main_language);
+        this.nationality.setValue(this.user.nationality);
+        this.recitations.setValue(this.user.recitations); 
+      } 
+      else {
         // Handle case for creating a new user
-        this.initializeForm();
+        console.log("new user case")
       }
   };
-
-//   // Method to populate the form with user data
-//   private patchForm(user: User): void {
-//     this.userForm.patchValue({
-//     first_name: user.first_name,
-//     middle_name: user.middle_name,
-//     last_name: user.last_name,
-//     email: user.email,
-//     date_of_birth: user.date_of_birth,
-//     is_activated: user.is_activated, 
-//     profile_img_url: user.profile_img_url,
-//     phone_number: user.phone_number,
-//     gender: user.gender, 
-//     main_language: user.main_language,
-//     nationality: user.nationality,
-//     recitations: user.recitations,
-//   });
-// }
-
-// Method to initialize a blank form
-private initializeForm(): void {
-
-}
-  private loadUsers() { // Fetch data from the JSON file
-    this.http.get<User[]>(this.url_to_db ).subscribe(
-      data => {
-        this.users = data;
-      },
-    );
-  }
-
 }
---
+++ src/app/services/user-retrieve.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../interfaces/user';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserRetrieveService {
+  users_url = './assets/db/users.json'
+
+  constructor(private http: HttpClient) {};
+
+  getUserById(userId: number): Observable<User | undefined> {
+    return this.http.get<User[]>(this.users_url).pipe(
+      map(users => users.find(user => user.id === userId))
+    );
+  }
+}
