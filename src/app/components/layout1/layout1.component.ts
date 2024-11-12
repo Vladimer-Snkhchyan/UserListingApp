d71879a7d12c4909e96fe85e3db6836bd9423541
Vladimer-Snkhchyan <vladimir.snkhchyan@optimumpartners.co>
2024-11-12 13:15:51 +0400

    directive fix s

--- src/app/components/layout1/layout1.component.ts
+++ src/app/components/layout1/layout1.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TopBarComponent } from '../../general/top-bar/top-bar.component';
+import { TopBarComponent } from '../top-bar/top-bar.component';
 
 @Component({
   selector: 'app-layout1',
--- src/app/tests/layout1.component.spec.ts
+++ src/app/tests/layout1.component.spec.ts
@@ -1,15 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterOutlet } from '@angular/router';
-import { Layout1Component } from '../common_ui/layout1/layout1.component';
-import { TopBarComponent } from '../general/top-bar/top-bar.component';
+import { Layout1Component } from '../components/layout1/layout1.component';
 import { RouterTestingModule } from '@angular/router/testing';
+
 describe('Layout1Component', () => {
   let component: Layout1Component;
   let fixture: ComponentFixture<Layout1Component>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterOutlet,RouterTestingModule, TopBarComponent],
+      imports: [RouterOutlet,RouterTestingModule],
       providers: [Layout1Component]
   }).compileComponents();
 
